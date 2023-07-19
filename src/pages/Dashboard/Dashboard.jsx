5418b78de7f5f70293b15d3112f54be06f526289
Sumon <m.sumon@outlook.com>
2023-07-19 09:04:25 +0600

    delete handeled and refetch

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,42 +1,44 @@
-import { useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import Modal from "../../components/Modal/Modal";
 import UpdateModal from "../../components/Modal/UpdateModal";
 import { useQuery } from "react-query";
 
-
 const Dashboard = () => {
-  
-  const [house,setHouse] = useState([]);
+  const [house, setHouse] = useState([]);
 
-  
-  const { data:houses} = useQuery("houses", () =>
+  const { data: houses, refetch } = useQuery("houses", () =>
     fetch(`http://localhost:5000/api/v1/owners/houses`).then((res) =>
       res.json()
     )
   );
   const handleDelete = async (houseId) => {
     try {
-      await axios.delete(`http://localhost:5000/api/v1/owners/deleteHouse/${houseId}`);
-      console.log('house deleted');
+      await axios.delete(
+        `http://localhost:5000/api/v1/owners/deleteHouse/${houseId}`
+      );
+      console.log("house deleted");
     } catch (error) {
       console.log(error);
     }
+    refetch();
   };
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6 mt-12 text-center">
-        Owned Houses
-      </h1>
-      {/* <div className="">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-blue-500"
+    <div className="p-4 mt-24">
+      <div className="flex justify-between items-center w-full mb-4">
+        <h1 className="text-3xl font-bold text-center flex-1">Owned Houses</h1>
+        <div className="flex">
+          <label
+            htmlFor="house-modal"
+            className="btn btn-lg btn-primary text-white font-bold mb-4"
           >
-            Add House
-          </button>
-        </div> */}
+            Add New House
+          </label>
+          <Modal></Modal>
+          <UpdateModal house={house} setHouse={setHouse}></UpdateModal>
+        </div>
+      </div>
       <table className="w-full bg-white border border-gray-200">
         <thead>
           <tr>
@@ -68,12 +70,12 @@ const Dashboard = () => {
               <td className="p-4">{house.description}</td>
               <td className="p-4">
                 <button
-                  className="bg-red-500 text-white py-2 px-4 rounded mr-2"
+                  className="bg-red-500 text-white py-4 px-4 rounded w-20 mb-2"
                   onClick={() => handleDelete(house._id)}
                 >
                   Delete
                 </button>
-                <button className="bg-blue-500 text-white py-2 px-4 rounded">
+                <button className="bg-blue-500 text-white ml-4 py-4 px-4 rounded w-20">
                   Edit
                 </button>
               </td>
@@ -81,14 +83,6 @@ const Dashboard = () => {
           ))}
         </tbody>
       </table>
-      <label
-              htmlFor="house-modal"
-              className="btn btn-sm btn-success text-white font-bold mt-2"
-            >
-              Add New House
-            </label>
-            <Modal></Modal>
-            <UpdateModal house={house} setHouse={setHouse}></UpdateModal>
     </div>
   );
 };
